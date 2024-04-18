1b3ef80a08a77e42435e6f9e93758f2524c2b0e3
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-04-18 22:03:33 +0000

    added html & img & js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: yoocha Han
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+function CalculateTrapezoid() {
+  const top = parseFloat(document.getElementById("get_top").value)
+  const base = parseFloat(document.getElementById("get_base").value)
+  const height = parseFloat(document.getElementById("get_height").value)
+  // caculate
+  let answer = 1/2 * (top + base)*height
+  document.getElementById("Answer").innerHTML = "area of trapezoid is " + answer + "cm³."
+}
